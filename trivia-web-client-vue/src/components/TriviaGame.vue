7d357fd81b2b0feadd9f2e53bee1aad52106f432
Krasimir Hristozov <krasi.hristozov@gmail.com>
2018-11-27 22:58:15 +0200

    Displaying the list of players

--- trivia-web-client-vue/src/components/TriviaGame.vue
+++ trivia-web-client-vue/src/components/TriviaGame.vue
@@ -1,6 +1,54 @@
 <template>
-    <h1>This is the trivia game</h1>
+    <div>
+        <div v-if="isLoading">Loading players...</div>
+        <div v-else>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Answers</th>
+                    <th>Points</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <template v-for="player in players">
+                    <tr v-bind:key="player.id">
+                        <td>{{ player.id }}</td>
+                        <td>{{ player.name }}</td>
+                        <td>{{ player.answers }}</td>
+                        <td>{{ player.points }}</td>
+                        <td>Action buttons</td>
+                    </tr>
+                </template>
+            </tbody>
+        </table>
+        <a class="button is-primary">Add Player</a>
+        </div>
+    </div>
 </template>
 
 <script>
+import axios from 'axios'
+import { API_BASE_URL } from '../config'
+
+export default {
+    data() {
+        return {
+            isLoading: true,
+            players: {}
+        }
+    },
+    async created () {
+        axios.defaults.headers.common['Authorization'] = `Bearer ${await this.$auth.getAccessToken()}`
+        try {
+            const response = await axios.get(API_BASE_URL + '/players')
+            this.players = response.data.data
+            this.isLoading = false
+        } catch (e) {
+            // handle the authentication error here
+        }
+    }
+}
 </script>
